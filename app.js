d4430d494e55c208a510bb07fc41505c122abeee
Sola Kabuta <solakabuta@gmail.com>
2025-03-03 15:56:34 +0100

    update: README.md

--- app.js
+++ app.js
@@ -12,13 +12,16 @@ document
     if (!emailRegex.test(mail.value.trim())) {
       errorMessage.style.display = "block";
       mail.style.border = "1px solid hsl(4, 100%, 67%)";
+    //  SECTION BELOW NEED TO BE FIXED
       mail.style.background = "hsl(4, 100%, 97%)";
-      
+      mail.classList.add('mail')
+      mail.value.style.color = 'hsl(4, 100%, 67%)'
       return; // Bloquer la redirection si email invalide
     } else {
       errorMessage.style.display = "none";
       mail.style.border = ""; 
       mail.style.background = "";
+      mail.classList.remove('mail')
     }
 
     // Rediriger vers success.html avec l’email dans l’URL (query string)
